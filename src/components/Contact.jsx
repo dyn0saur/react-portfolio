c7869dc8dabb38a6eb44aebc9f095469005647b5
dyn0saur <112749722+dyn0saur@users.noreply.github.com>
2025-08-12 20:13:09 +0900

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ export default function Contact(){
   return (
     <section id="contact" className="container">
       <h2>Contact</h2>
-      <p>mail: <a href="mailto:dyn0saur.527@gmail.com">dyn0saur.527@gmail.com</a></p>
+      <p>mail: <a href="mailto:contact@geonjukim.com">contact@geonjukim.com</a></p>
       <p>blog: <a href="https://blog.naver.com/dyn0saur">blog.naver.com/dyn0saur</a></p>
       <p>phone:</p>     
     </section>
